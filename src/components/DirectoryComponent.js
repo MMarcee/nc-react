01ccab9894a0954714ed9d021905b4e6b36a4df9
MMarcee <marbouame@gmail.com>
2021-02-05 17:20:28 -0500

    Week 3 updates: Router Exercise

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component {
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+function RenderDirectoryItem({campsite}) {
+    return (
+        <Card>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+
+function Directory(props){
+    
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} />
+                    
+            </div>
+        );
+    });
 
         return (
             <div className="container">
@@ -24,7 +31,7 @@ class Directory extends Component {
                 
             </div>
         );
-    }
+  
 }
 
 export default Directory;
