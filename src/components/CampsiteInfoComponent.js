a9cae8683c3301356533d06bb76a4bb603f92f08
MMarcee <marbouame@gmail.com>
2021-02-14 17:00:04 -0500

    Week 4 Workshop Update

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,101 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, 
+    Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
+import React, { Component } from 'react';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+class CommentForm extends Component {
+    constructor(props) {
+         super(props);
+
+         this.state = {
+            rating: "",
+            author: "",
+            text: "",
+            touched: {
+                rating: false,
+                author: false,
+                text: false,
+	            isModalOpen: false
+            }
+       };
+   
+         this.toggleModal = this.toggleModal.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
+     }
+     toggleModal() {
+         this.setState({
+             isModalOpen: !this.state.isModalOpen
+         });
+     }
+
+     handleSubmit(values) {
+        console.log('Current state is: ' + JSON.stringify(values));
+        alert('Current state is: ' + JSON.stringify(values));
+    }
+   
+     render() {
+         return (
+            <React.Fragment>
+             <Button outline onClick={this.toggleModal}><i className="fa fa-lg fa-pencil" />Submit Comment</Button>
+
+             <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                 <ModalHeader toggle={this.toggleModal}>Comments</ModalHeader>
+                 <ModalBody>
+                     <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                         <div className="form-group">
+                             <Label>Rating</Label>
+                             <Control.select model=".rating" name="rating" className="form-control">
+                                 <option>1</option>
+                                 <option>2</option>
+                                 <option>3</option>
+                                 <option>4</option>
+                                 <option>5</option>
+                             </Control.select>
+                         </div>        
+                         <div className="form-group">
+                             <Label>Your Name</Label>
+                             <Control.text model=".author" id="author" name="author"
+                                 placeholder="Your Name"
+                                 className="form-control"
+                                 validators={{
+                                 required,
+                                 minLength: minLength(2),
+                                 maxLength: maxLength(15)
+                                 }}
+                             />
+                             <Errors
+                                 className="text-danger"
+                                 model=".author"
+                                 show="touched"
+                                 component="div"
+                                 messages={{
+                                     required: 'Required',
+                                     minLength: 'Must be at least 2 characters',
+                                     maxLength: 'Must be 15 characters or less'
+                                 }}
+                             />
+                         </div>    
+                         <div className="form-group">
+                             <Label>Comment</Label>
+                             <Control.textarea model=".text" id="comment" name="comment"
+                                 rows="6"
+                                 className="form-control"
+                             />
+                            <Button type="submit" color="primary">Submit</Button>
+                         </div>
+                     </LocalForm>
+                 </ModalBody>
+             </Modal>
+         </React.Fragment> 
+        );
+    }
+}       
 
 function RenderCampsite({campsite}) {
     return(
@@ -27,8 +122,9 @@ function RenderComments({comments}) {
                         <p>{new Intl.DateTimeFormat('en-US', 
                            { year: 'numeric', month: 'short', day: '2-digit'})
                             .format(new Date(Date.parse(comment.date)))}</p>
-                    </div>
+                     </div>
                   )}
+                    <CommentForm />
             </div>
         );
     }
